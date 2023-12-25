4aecdbf3af3b7be2b70a3b21e7078d9964221ecb
nerzul88 <nerzul.88@gmail.com>
2023-12-25 16:57:15 +0300

    M3L2 Fixes

---
+++ Sources/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+
+import UIKit
+
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	// MARK: - Public properties
+
+	var window: UIWindow?
+
+	// MARK: - Lifecycle
+
+	func scene(
+		_ scene: UIScene,
+		willConnectTo session: UISceneSession,
+		options connectionOptions: UIScene.ConnectionOptions
+	) {
+		guard let scene = (scene as? UIWindowScene) else { return }
+		let window = UIWindow(windowScene: scene)
+
+		let navigationController = UINavigationController()
+		
+		window.rootViewController = navigationController
+		window.makeKeyAndVisible()
+
+		self.window = window
+	}
+}
