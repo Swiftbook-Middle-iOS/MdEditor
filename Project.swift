b3c645289971194d15af48702f8386eae4b006ed
BorisBakhlykov <boris.bakhlykov@gmail.com>
2023-12-27 19:38:39 +0500

    M3L202 - New bug fixes

--- Project.swift
+++ Project.swift
@@ -6,15 +6,17 @@ public var scripts: [TargetScript] {
 	let swiftLintScriptString = """
 								export PATH="$PATH:/opt/homebrew/bin"
 								if which swiftlint > /dev/null; then
-								swiftlint
+									swiftlint
 								else
 									echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 									exit 1
-									fi
+								fi 
 								"""
+								
 	let swiftLintScript = TargetScript.post(
 		script: swiftLintScriptString,
-		name: "SwiftLint", basedOnDependencyAnalysis: false
+		name: "SwiftLint",
+		basedOnDependencyAnalysis: false
 	)
 
 	scripts.append(swiftLintScript)
