22a5e917b92085ba3880088f6cd50dac5f6de2ab
BorisBakhlykov <boris.bakhlykov@gmail.com>
2023-12-28 16:41:12 +0500

    M3L301-Added Package

---
+++ TaskManagerPackage/Sources/TaskManagerPackage/TaskManager.swift
@@ -0,0 +1,33 @@
+//
+//  TaskManager.swift
+
+/// Предоставляет список заданий.
+final public class TaskManager {
+	private var taskList = [Task]()
+
+	public init() {}
+
+	public func allTasks() -> [Task] {
+		taskList
+	}
+
+	public func completedTasks() -> [Task] {
+		taskList.filter { $0.completed }
+	}
+
+	public func uncompletedTasks() -> [Task] {
+		taskList.filter { !$0.completed }
+	}
+
+	public func addTask(task: Task) {
+		taskList.append(task)
+	}
+
+	public func addTasks(tasks: [Task]) {
+		taskList.append(contentsOf: tasks)
+	}
+
+	public func removeTask(task: Task) {
+		taskList.removeAll { $0 === task }
+	}
+}
