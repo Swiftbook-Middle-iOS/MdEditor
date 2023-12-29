0e5357e86e3f2b9d92040e0ff9825dc5e31b00bb
BorisBakhlykov <boris.bakhlykov@gmail.com>
2023-12-29 19:06:24 +0500

    M3L302 - Update few tests and code style with markdowns

--- TaskManagerPackage/Tests/TaskManagerPackageTests/TaskManagerPackageTests.swift
+++ TaskManagerPackage/Tests/TaskManagerPackageTests/TaskManagerPackageTests.swift
@@ -2,43 +2,37 @@ import XCTest
 @testable import TaskManagerPackage
 
 final class TaskManagerPackageTests: XCTestCase {
-	///Создание тестового задания
-	let task = Task(title: "Test task")
 
-	///Инъекция класса TaskManager
-	var sut: TaskManager!
+	// MARK: - Public properties
+	///Создание тестового задания.
+	private let task = Task(title: "Test task")
 
-	///Создание нескольких тестовых заданий
-	var tasks: [Task] {
+	///Создание нескольких заданий с разными статусами выполнения.
+	private let tasksToCheckCompletion = [
+		Task(title: "Test task", completed: true),
+		Task(title: "Test task", completed: false)
+	]
+
+	///Создание нескольких тестовых заданий.
+	private var tasks: [Task] {
 		[task, task]
 	}
 
-	///Создание нескольких выполненных тестовых заданий
-	var completedTasks: [Task] {
-		[
-			Task(title: "Test task", completed: true),
-			Task(title: "Test task", completed: true)
-		]
-	}
-
-	///Создание нескольких не выполненных тестовых заданий
-	var uncompletedTasks: [Task] {
-		[
-			Task(title: "Test task", completed: false),
-			Task(title: "Test task", completed: false)
-		]
-	}
-
-	///Создание переменной возвращающей результат проверки на отсутствие заданий
-	var isEmpty: Bool {
+	///Создание переменной возвращающей результат проверки на отсутствие заданий.
+	private var isEmpty: Bool {
 		sut.allTasks().isEmpty
 	}
 
-	///Создание переменной возвращающей количество заданий
-	var count: Int {
+	///Создание переменной возвращающей количество заданий.
+	private var count: Int {
 		sut.allTasks().count
 	}
 
+	// MARK: - Dependencies
+	///Инъекция класса TaskManager. (sut - System Under Testing)
+	private var sut: TaskManager!
+
+	// MARK: - Lifecycle
 	override func setUp() {
 		super.setUp()
 		sut = TaskManager()
@@ -49,15 +43,16 @@ final class TaskManagerPackageTests: XCTestCase {
 		super.tearDown()
 	}
 
-	///Тестирование функции добавления одного задания
-	func test_addOneTask_shouldBeSuccess() {
+	// MARK: - Public methods
+	///Тестирование функции добавления одного задания.
+	func test_addTask_whenAddingSingleTask_shouldBeSuccess() {
 		sut.addTask(task: task)
 
 		XCTAssertFalse(isEmpty, "Результат проверки отсутствия задания должен быть - False")
 	}
 
-	///Тестирование функции добавления нескольких заданий
-	func test_addOneTasks_shouldBeSuccess() {
+	///Тестирование функции добавления нескольких заданий.
+	func test_addTasks_whenAddingMultipleTasks_shouldBeSuccess() {
 		sut.addTasks(tasks: tasks)
 
 		let result = count > 1
@@ -66,8 +61,8 @@ final class TaskManagerPackageTests: XCTestCase {
 		XCTAssertTrue(result, "Результат проверки добавления задания должен быть - True")
 	}
 
-	///Тестирование функции получения всех заданий
-	func test_getAllTasks_shouldBeSuccess() {
+	///Тестирование функции получения всех заданий.
+	func test_allTasks_whenTasksAdded_shouldBeSuccess() {
 		sut.addTasks(tasks: tasks)
 
 		let result = tasks.count == sut.allTasks().count
@@ -76,31 +71,37 @@ final class TaskManagerPackageTests: XCTestCase {
 		XCTAssertTrue(result, "Результат проверки получения заданий должен быть - True")
 	}
 
-	///Тестирование функции удаления одного задания
-	func test_removeTask_shouldBeSuccess() {
+	///Тестирование функции удаления одного задания.
+	func test_removeTask_whenTaskRemoved_shouldBeSuccess() {
 		sut.addTask(task: task)
 		sut.removeTask(task: task)
 
 		XCTAssertTrue(isEmpty, "Результат проверки отсутствия задания после удаления должен быть - True")
 	}
-	
-	///Тестирование функции получения всех выполненных заданий
-	func test_getCompletedTasks_shouldBeSuccess() {
-		sut.addTasks(tasks: completedTasks)
 
-		let result = sut.completedTasks().contains(where: { $0.completed })
+	///Тестирование функции получения всех выполненных заданий.
+	func ttest_completedTasks_whenTasksAdded_shouldBeSuccess() {
+		sut.addTasks(tasks: tasksToCheckCompletion)
+
+		let result = sut.uncompletedTasks()
 
 		XCTAssertFalse(isEmpty, "Результат проверки отсутствия задания должен быть - False")
-		XCTAssertTrue(result, "Результат наличия выполненных заданий должен быть - True")
+		XCTAssertFalse(
+			result.contains(where: { $0.completed }),
+			"Результат проверки наличия выполненных заданий должен быть - False"
+		)
 	}
 
-	///Тестирование функции получения всех не выполненных заданий
-	func test_getUncompletedTasks_shouldBeSuccess() {
-		sut.addTasks(tasks: uncompletedTasks)
+	///Тестирование функции получения всех не выполненных заданий.
+	func test_getUncompletedTasks_whenTasksAdded_shouldBeSuccess() {
+		sut.addTasks(tasks: tasksToCheckCompletion)
 
-		let result = sut.uncompletedTasks().contains(where: { !$0.completed })
+		let result = sut.uncompletedTasks()
 
 		XCTAssertFalse(isEmpty, "Результат проверки отсутствия задания должен быть - False")
-		XCTAssertTrue(result, "Результат наличия не выполненных заданий должен быть - True")
+		XCTAssertFalse(
+			result.contains(where: { $0.completed }),
+			"Результат проверки наличия выполненных заданий должен быть - False"
+		)
 	}
 }
