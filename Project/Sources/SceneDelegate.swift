4a49a6b5f7b93bf5c18480875d22bf0ac3374203
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2024-01-27 22:16:13 +0400

    Reworked ICoordinator protocol, added BaseCoordinator class for coordinators with child coordinators

--- Project/Sources/SceneDelegate.swift
+++ Project/Sources/SceneDelegate.swift
@@ -8,10 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     // MARK: Public properties
     var window: UIWindow?
-
-    // MARK: Dependencies
-    private let taskManager = TaskManager()
-    private var appCoordinator: IAppCoordinator! // swiftlint:disable:this implicitly_unwrapped_optional
+    private var appCoordinator: ICoordinator! // swiftlint:disable:this implicitly_unwrapped_optional
 
     func scene(
         _ scene: UIScene,
@@ -21,23 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: scene)
 
-        let navigationController = UINavigationController()
-
-        appCoordinator = AppCoordinator(navigationController: navigationController, taskManager: buildTaskManager())
+        appCoordinator = AppCoordinator(window: window)
         appCoordinator.start()
 
-        window.rootViewController = navigationController
-        window.makeKeyAndVisible()
-
         self.window = window
-    }
-
-    private func buildTaskManager() -> ITaskManager {
-        let taskManager = TaskManager()
-        let repository = TaskRepositoryStub()
-        let orderedTaskManager = OrderedTaskManager(taskManager: taskManager)
-        orderedTaskManager.addTasks(tasks: repository.getTasks())
-
-        return orderedTaskManager
     }
 }
