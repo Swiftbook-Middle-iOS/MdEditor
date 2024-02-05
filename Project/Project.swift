42088e20efa65d5806865f14b9d45e8f2f6dc50d
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2024-02-05 21:14:12 +0400

    Added Assets folder to bundle resources, instead of separate files

--- Project/Project.swift
+++ Project/Project.swift
@@ -12,14 +12,14 @@ enum ProjectSettings {
 
 public var swiftLintTargetScript: TargetScript {
 	let swiftLintScriptString = """
-								export PATH="$PATH:/opt/homebrew/bin"
-								if which swiftlint > /dev/null; then
-									swiftlint
-								else
-									echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-									exit 1
-								fi
-								"""
+		export PATH="$PATH:/opt/homebrew/bin"
+		if which swiftlint > /dev/null; then
+		 swiftlint
+		else
+		 echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+		 exit 1
+		fi
+		"""
 
 	return TargetScript.pre(
 		script: swiftLintScriptString,
@@ -33,61 +33,61 @@ private let myScripts: [TargetScript] = [
 ]
 
 let target = Target(
-    name: ProjectSettings.projectName,
-    platform: .iOS,
-    product: .app,
-    bundleId: "ru.\(ProjectSettings.bundleId)",
-    deploymentTarget: .iOS(targetVersion: ProjectSettings.targetVersion, devices: .iphone),
-    infoPlist: "Info.plist",
-    sources: ["Sources/**", "Shared/**"],
-    resources: ["Resources/**", "Assets/**"],
-    scripts: myScripts,
-    dependencies: [
-        .package(product: "TaskManagerPackage")
-    ]
+	name: ProjectSettings.projectName,
+	platform: .iOS,
+	product: .app,
+	bundleId: "ru.\(ProjectSettings.bundleId)",
+	deploymentTarget: .iOS(targetVersion: ProjectSettings.targetVersion, devices: .iphone),
+	infoPlist: "Info.plist",
+	sources: ["Sources/**", "Shared/**"],
+	resources: ["Resources/**", .folderReference(path: "Assets", tags: [], inclusionCondition: nil)],
+	scripts: myScripts,
+	dependencies: [
+		.package(product: "TaskManagerPackage")
+	]
 )
 
 let testTarget = Target(
-    name: "\(ProjectSettings.projectName)Tests",
-    platform: .iOS,
-    product: .unitTests,
-    bundleId: "ru.\(ProjectSettings.bundleId)Tests",
-    deploymentTarget: .iOS(targetVersion: ProjectSettings.targetVersion, devices: .iphone),
-    infoPlist: .none,
-    sources: ["Tests/**", "Shared/**"],
-    resources: ["Resources/**"],
-    scripts: myScripts,
-    dependencies: [
-        /* Target dependencies can be defined here */
-        /* .framework(path: "framework") */
-        .target(name: "\(ProjectSettings.projectName)")
-    ],
-    settings: .settings(base: ["GENERATE_INFOPLIST_FILE": "YES"])
+	name: "\(ProjectSettings.projectName)Tests",
+	platform: .iOS,
+	product: .unitTests,
+	bundleId: "ru.\(ProjectSettings.bundleId)Tests",
+	deploymentTarget: .iOS(targetVersion: ProjectSettings.targetVersion, devices: .iphone),
+	infoPlist: .none,
+	sources: ["Tests/**", "Shared/**"],
+	resources: ["Resources/**"],
+	scripts: myScripts,
+	dependencies: [
+		/* Target dependencies can be defined here */
+		/* .framework(path: "framework") */
+		.target(name: "\(ProjectSettings.projectName)")
+	],
+	settings: .settings(base: ["GENERATE_INFOPLIST_FILE": "YES"])
 )
 
 let uiTestTarget = Target(
-    name: "\(ProjectSettings.projectName)UITests",
-    platform: .iOS,
-    product: .uiTests,
-    bundleId: "ru.\(ProjectSettings.bundleId)UITests",
-    deploymentTarget: .iOS(targetVersion: ProjectSettings.targetVersion, devices: .iphone),
-    infoPlist: .none,
-    sources: ["UITests/Sources/**", "Shared/**"],
-    resources: ["Resources/**"],
-    scripts: myScripts,
-    dependencies: [
-        .target(name: "\(ProjectSettings.projectName)")
-    ],
-    settings: .settings(base: ["GENERATE_INFOPLIST_FILE": "YES"])
+	name: "\(ProjectSettings.projectName)UITests",
+	platform: .iOS,
+	product: .uiTests,
+	bundleId: "ru.\(ProjectSettings.bundleId)UITests",
+	deploymentTarget: .iOS(targetVersion: ProjectSettings.targetVersion, devices: .iphone),
+	infoPlist: .none,
+	sources: ["UITests/Sources/**", "Shared/**"],
+	resources: ["Resources/**"],
+	scripts: myScripts,
+	dependencies: [
+		.target(name: "\(ProjectSettings.projectName)")
+	],
+	settings: .settings(base: ["GENERATE_INFOPLIST_FILE": "YES"])
 )
 
 let project = Project(
 	name: ProjectSettings.projectName,
 	organizationName: ProjectSettings.organizationName,
-    options: .options(
-        defaultKnownRegions: ["en", "ru"],
-        developmentRegion: "en"
-    ),
+	options: .options(
+		defaultKnownRegions: ["en", "ru"],
+		developmentRegion: "en"
+	),
 	packages: [
 		.local(path: .relativeToManifest("../Packages/TaskManagerPackage"))
 	],
@@ -101,28 +101,28 @@ let project = Project(
 		defaultSettings: .recommended()
 	),
 	targets: [target, testTarget, uiTestTarget],
-    schemes: [
-        Scheme(
-            name: "MdEditor",
-            shared: true,
-            buildAction: .buildAction(targets: [TargetReference(stringLiteral: ProjectSettings.projectName)]),
-            testAction: .targets(["\(ProjectSettings.projectName)Tests"]),
-            runAction: .runAction(executable: TargetReference(stringLiteral: ProjectSettings.projectName))
-        ),
-        Scheme(
-            name: "MdEditorTests",
-            shared: true,
-            buildAction: .buildAction(targets: [TargetReference(stringLiteral: "\(ProjectSettings.projectName)Tests")]),
-            testAction: .targets(["\(ProjectSettings.projectName)Tests"]),
-            runAction: .runAction(executable: TargetReference(stringLiteral: "\(ProjectSettings.projectName)Tests"))
-        ),
-        Scheme(
-            name: "MdEditorUITests",
-            shared: true,
-            buildAction: .buildAction(targets: [TargetReference(stringLiteral: "\(ProjectSettings.projectName)UITests")]),
-            testAction: .targets(["\(ProjectSettings.projectName)UITests"]),
-            runAction: .runAction(executable: TargetReference(stringLiteral: "\(ProjectSettings.projectName)UITests"))
-        )
-    ],
-    resourceSynthesizers: [.strings()]
+	schemes: [
+		Scheme(
+			name: "MdEditor",
+			shared: true,
+			buildAction: .buildAction(targets: [TargetReference(stringLiteral: ProjectSettings.projectName)]),
+			testAction: .targets(["\(ProjectSettings.projectName)Tests"]),
+			runAction: .runAction(executable: TargetReference(stringLiteral: ProjectSettings.projectName))
+		),
+		Scheme(
+			name: "MdEditorTests",
+			shared: true,
+			buildAction: .buildAction(targets: [TargetReference(stringLiteral: "\(ProjectSettings.projectName)Tests")]),
+			testAction: .targets(["\(ProjectSettings.projectName)Tests"]),
+			runAction: .runAction(executable: TargetReference(stringLiteral: "\(ProjectSettings.projectName)Tests"))
+		),
+		Scheme(
+			name: "MdEditorUITests",
+			shared: true,
+			buildAction: .buildAction(targets: [TargetReference(stringLiteral: "\(ProjectSettings.projectName)UITests")]),
+			testAction: .targets(["\(ProjectSettings.projectName)UITests"]),
+			runAction: .runAction(executable: TargetReference(stringLiteral: "\(ProjectSettings.projectName)UITests"))
+		)
+	],
+	resourceSynthesizers: [.strings()]
 )
