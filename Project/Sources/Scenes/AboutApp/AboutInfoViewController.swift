f92e7f190332f39e9f5a742cedeb0a9ee61c4ac8
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2024-02-12 22:27:58 +0400

    Added webview to render about.md file

--- Project/Sources/Scenes/AboutApp/AboutInfoViewController.swift
+++ Project/Sources/Scenes/AboutApp/AboutInfoViewController.swift
@@ -7,54 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
-final class AboutAppViewController: UIViewController {
+final class AboutAppViewController: UIViewController, WKNavigationDelegate {
 
-	private lazy var label: UILabel = makeLabel(text: labelText ?? "")
-	var labelText: String?
+	// MARK: Dependencies
+	var mdText: String! // swiftlint:disable:this implicitly_unwrapped_optional
+	var markdownCoverter: IMarkdownToHtmlConverter! // swiftlint:disable:this implicitly_unwrapped_optional
 
+	// MARK: Private properties
+	private var webView = WKWebView()
+
+	// MARK: Lifecyle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		setupUI()
+		webView.navigationDelegate = self
+		view = webView
+
+		let html = markdownCoverter.convert(mdText)
+		loadPage(html: html)
 	}
 
-	override func viewWillLayoutSubviews() {
-		super.viewWillLayoutSubviews()
-		layout()
-	}
-}
-
-// MARK: Setup UI
-private extension AboutAppViewController {
-	func setupUI() {
-		view.backgroundColor = Theme.white
-		view.addSubview(label)
-		title = L10n.AboutApp.title
+	// MARK: Private methods
+	private func loadPage(url: URL) {
+		webView.load(URLRequest(url: url))
+		webView.allowsBackForwardNavigationGestures = true
 	}
 
-	func makeLabel(text: String) -> UILabel {
-		let label = UILabel()
-		label.textColor = Theme.black
-		label.text = text
-		label.numberOfLines = 0
-		label.font = UIFont.preferredFont(forTextStyle: .body)
-		label.lineBreakMode = .byWordWrapping
-		label.adjustsFontForContentSizeCategory = true
-		label.translatesAutoresizingMaskIntoConstraints = false
-		return label
-	}
-}
-
-// MARK: Layout
-private extension AboutAppViewController {
-	func layout() {
-		NSLayoutConstraint.activate([
-			label.topAnchor.constraint(
-				equalTo: navigationController?.navigationBar.bottomAnchor ?? view.topAnchor,
-				constant: Sizes.Padding.double
-			),
-			label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Sizes.Padding.normal),
-			label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Sizes.Padding.normal)
-		])
+	private func loadPage(html: String) {
+		webView.loadHTMLString(html, baseURL: nil)
+		webView.allowsBackForwardNavigationGestures = false
 	}
 }
